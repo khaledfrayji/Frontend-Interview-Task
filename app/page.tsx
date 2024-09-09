b2b4a59d565bd57aa78205d7e7e1ca368aaf491b
khaled frayji <121765804+khaledfrayji@users.noreply.github.com>
2024-09-09 03:36:08 +0300

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import WeBelieveComponent from "@/components/BrightFuture";
 import Carousel from "@/components/Carousel";
 import ContactUs from "@/components/Contact";
-import ContactPawan from "@/components/ContactPawan";
-import Footer from "@/components/Footer";
 import FounderCard from "@/components/FounderCard";
 import OurStoryAndMission from "@/components/Mission";
-import Navbar from "@/components/Navbar";
-import QuickContact from "@/components/QuickContact";
 import OurServices from "@/components/Services";
 import SlidingComponent from "@/components/SlidingComponent";
 
